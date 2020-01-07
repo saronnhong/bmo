249575402ddaaf7953a24610d3db69b992db5630
Saron <saronnhong@gmail.com>
2020-01-07 14:58:44 -0800

    starting todo

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="bmo">
+        <div className="row">
+          <div className="col-md-2 menuItems">
+            <div className="taskList">Task List</div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="col-md-10 componentArea">
+            area 2
+        </div>
+        </div>
+
+
       </div>
     );
   }
---
+++ client/src/components/Todo/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Todo(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Todo;
