df98518fef84161c7e03d16bcc691a1c10749498
Saron <saronnhong@gmail.com>
2020-01-07 15:51:20 -0800

    add layout for todo list

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Todo from "./components/Todo";
 
 
 class App extends Component {
@@ -7,15 +8,13 @@ class App extends Component {
     return (
       <div className="bmo">
         <div className="row">
-          <div className="col-md-2 menuItems">
-            <div className="taskList">Task List</div>
+          <div className="col-md-3 menuItems">
+            <div className="taskList">Task List 1</div>
+          </div>
+          <div className="col-md-9 componentArea">
+            <Todo />
+          </div>
         </div>
-          <div className="col-md-10 componentArea">
-            area 2
-        </div>
-        </div>
-
-
       </div>
     );
   }
--- client/src/components/Todo/Todo.js
+++ client/src/components/Todo/Todo.js
@@ -1,10 +1,52 @@
 import React from "react";
 import "./style.css";
 
-function Todo(props) {
+function Todo() {
     return (
-        <div>
-            
+        <div className="container">
+            <h1>Task List</h1>
+            <p>Tasks are assigned to one or more people and can include priority and due date.</p>
+            <p>Looking for a way to track tasks in context of a larger product roadmap? Check out the Roadmap template.</p>
+            <p>↓ Click By Status to organize by assignee, priority, or due date.</p>
+            <br></br>
+            <table class="table table-sm">
+                <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Priority</th>
+                        <th scope="col">Date Created</th>
+                        <th scope="col">Due Date</th>
+                        <th scope="col">Comments</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">Create Todo List</td>
+                        <td>In Progress</td>
+                        <td>Medium</td>
+                        <td>1-7-20</td>
+                        <td>1-10-20</td>
+                        <td>No comments</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Create Database with SQL</td>
+                        <td>Not Started</td>
+                        <td>High</td>
+                        <td>1-8-20</td>
+                        <td>1-8-20</td>
+                        <td>No comments</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">CRUD implementation for Table</td>
+                        <td>Not Started</td>
+                        <td>High</td>
+                        <td>1-9-20</td>
+                        <td>1-9-20</td>
+                        <td>No comments</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
